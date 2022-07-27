d395f2d7d76fdaa9d1a1ba6a694d13c5533e23fd
shota4214 <seshiruff4214@yahoo.co.jp>
2022-07-27 10:30:30 +0900

    コード確認

--- db/schema.rb
+++ db/schema.rb
@@ -2,11 +2,11 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2018_10_05_035232) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "agendas", force: :cascade do |t|
+  create_table "agendas", force: :cascade do |t| #議題
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2018_10_05_035232) do
     t.index ["user_id"], name: "index_agendas_on_user_id"
   end
 
-  create_table "articles", force: :cascade do |t|
+  create_table "articles", force: :cascade do |t| #記事
     t.bigint "user_id"
     t.bigint "team_id"
     t.string "title", null: false
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2018_10_05_035232) do
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
-  create_table "assigns", force: :cascade do |t|
+  create_table "assigns", force: :cascade do |t| #割り当てる
     t.bigint "user_id", null: false
     t.bigint "team_id", null: false
     t.datetime "created_at", null: false
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2018_10_05_035232) do
     t.index ["user_id"], name: "index_assigns_on_user_id"
   end
 
-  create_table "comments", force: :cascade do |t|
+  create_table "comments", force: :cascade do |t| #コメント
     t.bigint "article_id"
     t.text "content"
     t.bigint "user_id"
